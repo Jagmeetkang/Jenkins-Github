ce326d58872d09ef494feb72d554cb833505b6a4
Jagmeetkang <Jagmeetkang01@gmail.com>
2025-03-06 16:13:58 -0500

    second commit

--- src/main/java/practice/JenkinsGithub.java
+++ src/main/java/practice/JenkinsGithub.java
@@ -3,5 +3,6 @@ package practice;
 public class JenkinsGithub {
     public static void main(String[] args) {
         System.out.println("Jenkins Github API");
+        System.out.println("Jenkins Github API second line");
     }
 }
